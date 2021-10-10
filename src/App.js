fbe68ba45064f82329755eeaaf3c05cb088deaf7
unknown <Sidharath0794.cse19@chitkara.edu.in>
2021-10-11 02:22:43 +0530

    Restructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './static/App.css';
+import Calculator from './components/calculator'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Calculator/>
       </header>
     </div>
   );
